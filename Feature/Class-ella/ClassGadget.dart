eafc06c7716ae83bde188eb5e15a614e1cfd92f0
MeCaOOP <ellagracetemblor@gmail.com>
2025-08-13 22:12:55 +0800

    Create ClassGadget.dart

---
+++ Feature/Class-ella/ClassGadget.dart
@@ -0,0 +1,10 @@
+class Gadget {
+  string Gadget_name;
+  char Gadget_color;
+
+  Gadget(this.Gadget_name, this.Gadget_color);
+
+  void introduce() {
+    print("Laptop $Gadget_name, \n $Gadget_color Purple");
+  }
+}
