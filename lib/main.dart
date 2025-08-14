d0edb875cb17bb347ce8b036928b269cf3799a99
binnooy <vin30149@gmail.com>
2025-08-14 16:18:19 +0800

    mao naba ni?

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 //import 'human.dart';
-
 //void main() {
   //var human = Human(
-   // 'Vantablack is so powerful that once its placed on a three-dimensional object it practically eliminates the third dimension tricking the human eye to think that its observing the two-dimensional object.',
-   // 'mao imo color Vantablack'
-  //);
+   //'Vantablack is so powerful that once its placed on a three-dimensional object it practically eliminates the third dimension tricking the human eye to think that its observing the two-dimensional object.',
+   //'mao imo color Vantablack'
+//  );
 //  human.describe();
 //}
 
 
-import 'Id.dart';
+//import 'Id.dart';
+//void main() {
+//var id = Id('Carla Thania', 19 );
+//id.describe();
+//}
+
+import 'gadget.dart';
 void main() {
-var id = Id('Carla Thania', 19 );
-id.describe();
+  var gadget = Gadget('Laptop', 'Burgundy Black');
+
+gadget.display();
 }
