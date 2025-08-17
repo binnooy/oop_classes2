4b6b1826dfb6ca8c33058f9894a01d0c7016b157
binnooy <vin30149@gmail.com>
2025-08-18 05:51:21 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
-//import 'human.dart';
-//void main() {
-  //var human = Human(
-   //'Vantablack is so powerful that once its placed on a three-dimensional object it practically eliminates the third dimension tricking the human eye to think that its observing the two-dimensional object.',
-   //'mao imo color Vantablack'
-//  );
-//  human.describe();
-//}
-
-
-//import 'Id.dart';
-//void main() {
-//var id = Id('Carla Thania', 19 );
-//id.describe();
-//}
-
+import 'human.dart';
+import 'Id.dart';
 import 'gadget.dart';
 void main() {
-  var gadget = Gadget('Laptop', 'Burgundy Black');
+  var human = Human(
+   'Vantablack is so powerful that once its placed on a three-dimensional object it practically eliminates the third dimension tricking the human eye to think that its observing the two-dimensional object.',
+   'mao imo color Vantablack'
+ );
+var id = Id('Carla Thania', 19 );
+var gadget = Gadget('Laptop', 'Burgundy Black');
 
+  
+human.describe();
+id.describe(); 
 gadget.display();
 }
